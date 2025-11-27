ecb55ae63995c281c4122a288853a53a917b3f1a
heman <hemantsahu56088@gmail.com>
2025-11-27 11:29:22 +0530

    retry

--- src/main/java/com/rising/Distributor/service/SmsService.java
+++ src/main/java/com/rising/Distributor/service/SmsService.java
@@ -2,22 +2,30 @@ package com.rising.Distributor.service;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsService {
-    private final String ACCOUNT_SID = "AC379c22082ca3fa8f1b8a1a43f4b414a3";
-    private final String AUTH_TOKEN = "0ccd7b0fbde00e4497f035b3a257ec04";
-    private final String FROM_PHONE = "+13018446159";
+    private final String accountSid;
+    private final String authToken;
+    private final String fromPhone;
 
-    public SmsService() {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    public SmsService(
+            @Value("${twilio.account-sid}") String accountSid,
+            @Value("${twilio.auth-token}") String authToken,
+            @Value("${twilio.from-phone}") String fromPhone) {
+        this.accountSid = accountSid;
+        this.authToken = authToken;
+        this.fromPhone = fromPhone;
+        Twilio.init(this.accountSid, this.authToken);
     }
 
     public void sendOtp(String toPhone, String otp) {
         Message.creator(
-                new com.twilio.type.PhoneNumber(toPhone),
-                new com.twilio.type.PhoneNumber(FROM_PHONE),
+                new PhoneNumber(toPhone),
+                new PhoneNumber(fromPhone),
                 "Your OTP code is: " + otp).create();
     }
 }
